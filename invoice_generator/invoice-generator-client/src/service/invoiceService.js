746951e6fc17d8cab63aea9faac6e6ba1b530a01
Kanimozhiparthiban1012 <kanimozhiparthiban12@gmail.com>
2026-02-26 17:13:29 +0530

    AppContext

--- invoice_generator/invoice-generator-client/src/service/invoiceService.js
+++ invoice_generator/invoice-generator-client/src/service/invoiceService.js
@@ -1,27 +1,20 @@
-// invoiceService.js
 import axios from "axios";
 
 export const saveInvoice = (baseURL, payload, token) => {
     return axios.post(`${baseURL}/invoices`, payload, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
+        headers: { Authorization: `Bearer ${token}` },
     });
 };
 
 export const getAllInvoices = (baseURL, token) => {
     return axios.get(`${baseURL}/invoices`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
+        headers: { Authorization: `Bearer ${token}` },
     });
 };
 
 export const deleteInvoice = (baseURL, id, token) => {
     return axios.delete(`${baseURL}/invoices/${id}`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
+        headers: { Authorization: `Bearer ${token}` },
     });
 };
 
@@ -32,8 +25,8 @@ export const sendInvoice = (baseURL, file, customerEmail, token) => {
 
     return axios.post(`${baseURL}/invoices/sendinvoice`, formData, {
         headers: {
-            Authorization: `Bearer ${token}`, // JWT for security
-            // Do NOT manually set Content-Type; Axios sets multipart boundaries automatically
+            Authorization: `Bearer ${token}`, // JWT
+            // do NOT manually set Content-Type for multipart
         },
     });
 };
